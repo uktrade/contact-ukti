3eb89688aabf3ade32c97c7aad53778dd7f5d626
Dom Smith <dom@teneightfive.com>
2016-01-12 14:23:12 +0000

    Make functional/integration tests run in node instead of ruby

---
+++ test/functional/wdio.wip.conf.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var baseConfig = require('./wdio.conf.js').config;
+
+// ============
+// Capabilities
+// ============
+baseConfig.capabilities = [{
+  browserName: 'chrome'
+}];
+
+// ===================
+// Test Configurations
+// ===================
+
+// run only work in progress tags
+baseConfig.cucumberOpts.tags = ['@wip', '~@ignore'];
+
+module.exports = baseConfig;
