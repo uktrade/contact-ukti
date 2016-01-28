2a074322f0ba18447d16cbefdb2d6b1c9a576ebd
Dom Smith <dom@teneightfive.com>
2016-01-28 14:30:14 +0000

    Update instructions for enquiry reason step

--- apps/contact-ukti/fields/enquiry.js
+++ apps/contact-ukti/fields/enquiry.js
@@ -3,5 +3,15 @@
 module.exports = {
   'enquiry-description': {
     validate: ['required'],
+    legend: {
+      className: 'visuallyhidden',
+      value: 'pages.company-location.header',
+    },
+    attributes: [
+      {
+        attribute: 'rows',
+        value: 5
+      },
+    ],
   },
 };
