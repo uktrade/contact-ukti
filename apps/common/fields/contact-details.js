c89ec1dc205ff80ee28423a2c5985139ff4a7c2d
Dom Smith <dom@teneightfive.com>
2016-03-23 17:40:31 +0000

    Update contact preferences Allow users to choose their preferred contact method rather than showing both with one being optional

--- apps/common/fields/contact-details.js
+++ apps/common/fields/contact-details.js
@@ -1,19 +1,40 @@
 'use strict';
 
 module.exports = {
+  'contact-preference': {
+    validate: ['required'],
+    className: 'form-group inline',
+    legend: {
+      className: 'form-label-bold',
+      value: 'fields.contact-preference.legend'
+    },
+    options: [
+      {
+        value: 'phone',
+        label: 'fields.contact-preference.options.phone.label',
+        toggle: 'phone-group',
+      },
+      {
+        value: 'email',
+        label: 'fields.contact-preference.options.email.label',
+        toggle: 'email-group',
+      },
+    ],
+  },
   email: {
     validate: ['required', 'email'],
     type: 'email',
     dependent: {
-      value: '',
-      field: 'no-email'
+      value: 'email',
+      field: 'contact-preference'
     }
-  },
-  'no-email': {
-    className: 'inline',
   },
   phone: {
     formatter: ['removespaces'],
-    validate: ['phonenumber'],
+    validate: ['required', 'phonenumber'],
+    dependent: {
+      value: 'phone',
+      field: 'contact-preference'
+    }
   },
 };
--- test/acceptance/features/pages/personal-details.js
+++ test/acceptance/features/pages/personal-details.js
@@ -18,13 +18,13 @@ var Page = function Page(client) {
       selector: '#fullname',
       value: 'My full name'
     },
+    preference: {
+      selector: '[name="contact-preference"]',
+      value: 'email'
+    },
     email: {
       selector: '#email',
       value: 'email@email.com'
-    },
-    phone: {
-      selector: '#phone',
-      value: '0123456789'
     }
   };
   var $errors = '.validation-error';
@@ -49,8 +49,8 @@ var Page = function Page(client) {
   this.complete = function completeForm() {
     return client
       .setValue(fields.fullname.selector, fields.fullname.value)
+      .click(fields.preference.selector + '[value="' + fields.preference.value + '"]')
       .setValue(fields.email.selector, fields.email.value)
-      .setValue(fields.phone.selector, fields.phone.value)
       .submitForm($form);
   };
 
