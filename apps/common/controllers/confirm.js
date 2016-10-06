68c406b69753fd78e60865b07073003cd697f896
Graham Veal <grahamveal@gmail.com>
2016-10-06 14:08:08 +0100

    Remove debug

--- apps/common/controllers/confirm.js
+++ apps/common/controllers/confirm.js
@@ -29,7 +29,6 @@ ConfirmController.prototype.saveValues = function saveValues(req, res, callback)
 
   BaseController.prototype.saveValues.call(this, req, res, function saveModel() {
     var data = _.pick(req.sessionModel.toJSON(), _.identity);
-    console.dir( data );
     var model = new Model(data);
     var service = serviceMap[req.originalUrl] && serviceMap[req.originalUrl](data);
 
